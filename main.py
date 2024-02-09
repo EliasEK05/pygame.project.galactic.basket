64366d5d5d8477f4134f5c3465201c57391942e6
Camelouch <93676537+Camelouch@users.noreply.github.com>
2024-02-09 15:33:56 +0100

    suite de l'affichage

--- main.py
+++ main.py
@@ -3,9 +3,15 @@ import pygame
 pygame.init()
 
 #dimension de l'écran de jeu
-ecran = pygame.display.set_mode((1000,700))
+ecran = pygame.display.set_mode((1000,700)) #pygame.FULLCREEN
+
 
 horloge = pygame.time.Clock()
+
+
+img = pygame.image.load("\prototype_jeu.jpg").convert_alpha()
+img = pygame.transform.scale(img,(200, 100))
+imge = img.get_rect()
 
 
 while True:
@@ -13,6 +19,12 @@ while True:
     horloge.tick(60)
     pygame.display.update()
 
+    ecran.blit(imge)
 
     # pour pouvoir utiliser les touches (à utiliser plus tard)
     pygame.event.get()
+
+
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            continuer = False
