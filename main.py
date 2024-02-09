07c626b4b37198c7d2d941e2b58010e83166bf90
Camelouch <93676537+Camelouch@users.noreply.github.com>
2024-02-09 15:02:52 +0100

    Affichage d'une fenêtre, dimension 1000 par 700 pixel (à revoir peut-être), écran noir pour l'instant.

--- main.py
+++ main.py
@@ -2,3 +2,17 @@ import pygame
 
 pygame.init()
 
+#dimension de l'écran de jeu
+ecran = pygame.display.set_mode((1000,700))
+
+horloge = pygame.time.Clock()
+
+
+while True:
+
+    horloge.tick(60)
+    pygame.display.update()
+
+
+    # pour pouvoir utiliser les touches (à utiliser plus tard)
+    pygame.event.get()
